cb8c2169a053ccbd325aee2c1bb62a8c0dbdc5b1
kierstenv <kierstenvicknair22@gmail.com>
2022-03-20 15:38:14 -0500

    animal queries

---
+++ server.js
@@ -0,0 +1,44 @@
+const { animals } = require('./data/animals.json');
+
+const express = require('express');
+const app = express();
+
+const filterByQuery = (query, animalsArray) => {
+  let personalityTraitsArray = [];
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+
+    personalityTraitsArray.forEach(trait => {
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  return filteredResults;
+};
+
+app.get('/api/animals', (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results)
+  }
+  res.json(results);
+});
+
+app.listen(3001, () => {
+  console.log('API server now on port 3001!');
+});

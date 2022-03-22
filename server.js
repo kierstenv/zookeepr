f7788a866577ce672201cef0578ad604b8c98bff
kierstenv <kierstenvicknair22@gmail.com>
2022-03-22 03:19:04 -0500

    animal id query

--- server.js
+++ server.js
@@ -29,12 +29,26 @@ const filterByQuery = (query, animalsArray) => {
   return filteredResults;
 };
 
+const findById = (id, animalsArray) => {
+  const result = animalsArray.filter(animal => animal.id === id)[0];
+  return result;
+};
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results)
   }
   res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(PORT, () => {

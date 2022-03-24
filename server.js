53fcb421056c9020789fef91d3c2c4c0438611e8
kierstenv <kierstenvicknair22@gmail.com>
2022-03-24 02:31:25 -0500

    serve app's front end

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals.json');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 const filterByQuery = (query, animalsArray) => {
   let personalityTraitsArray = [];
@@ -88,6 +89,22 @@ app.post('/api/animals', (req, res) => {
     const animal = createNewAnimal(req.body, animals);
     res.json(animal);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
